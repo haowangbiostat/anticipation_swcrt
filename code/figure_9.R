a186ad35e5aa3850d26f15c4a3bb141f9a61f540
haowangbiostat <haowangysph@gmail.com>
2025-04-12 13:05:15 -0400

    Add

--- code/figure_9.R
+++ code/figure_9.R
@@ -3,12 +3,19 @@ library(ggplot2)
 library(gridExtra)
 library(RColorBrewer)
 
-coeff_gamma <- function(phi, Q, j) {
-  numerator <- (2 * phi * Q^3 - 8 * phi * Q^2 * j + 5 * phi * Q^2 + Q^2 + 
-                6 * phi * Q * j^2 - 8 * phi * Q * j + 3 * phi * Q - 
-                4 * Q * j + Q + 3 * j^2 - j)
-  denominator <- Q * (Q - 1) * (phi * Q^2 - 2 * phi * Q + 2 * Q - 1)
-  return(numerator / denominator)
+coeff_HH <- function(phi, Q, j) {
+  numerator <- 6 * (j - Q - 1) * ((1 + 2 * phi * Q) * j - (1 + phi + phi * Q) * Q)
+  denominator <- Q * (Q + 1) * (phi * Q^2 + 2 * Q - phi * Q - 2)
+  result <- numerator / denominator
+  return(result)
+}
+
+df_HH_ANT <- function(phi, Q, j) {
+  numerator <- 6 * (phi * Q^3 - 3 * phi * Q^2 * j + phi * Q^2 + Q^2 + 
+                    2 * phi * Q * j^2 - 2 * phi * Q * j + phi * Q - 2 * Q * j + j^2)
+  denominator <- Q * (phi * Q^3 - 3 * phi * Q^2 + 2 * Q^2 + 2 * phi * Q - 3 * Q + 1)
+  result <- numerator / denominator
+  return(result)
 }
 
 run_simulation <- function(Q, expression_func) {
@@ -21,18 +28,20 @@ run_simulation <- function(Q, expression_func) {
 }
 
 Qs <- c(3, 5, 7)
-data_combined1 <- do.call(rbind, lapply(Qs, run_simulation, coeff_gamma))
+df_HH <- do.call(rbind, lapply(Qs, run_simulation, coeff_HH))
+df_HH_ANT <- do.call(rbind, lapply(Qs, run_simulation, df_HH_ANT))
 
-colors <- brewer.pal(n = 7, name = "Set1")
+custom_colors <- brewer.pal(n = 7, name = "Set1")
 
 generate_plot <- function(data, y_label, y_limits, facet_labels) {
   ggplot(data, aes(x = phi, y = value, color = j)) +
     geom_line() +
     scale_y_continuous(limits = y_limits, n.breaks = 4) +
     scale_x_continuous(breaks = seq(0, 1, by = 0.2), position = "top") +
-    scale_color_manual(values = colors) +
+    scale_color_manual(values = custom_colors) +
     labs(x = expression(phi), y = y_label, color = "j") +
-    facet_wrap(~ Q, ncol = 3, strip.position = "bottom", labeller = labeller(Q = facet_labels)) +
+    facet_wrap(~ Q, ncol = 3, strip.position = "bottom", 
+               labeller = labeller(Q = facet_labels)) +
     theme(legend.position = "bottom",
           legend.direction = "horizontal",
           legend.title = element_text(size = 12),
@@ -48,9 +57,14 @@ generate_plot <- function(data, y_label, y_limits, facet_labels) {
     guides(color = guide_legend(nrow = 1))
 }
 
-p1 <- generate_plot(data_combined1, 
-                    y_label = expression("Coefficients of " * delta), 
-                    y_limits = c(-0.5, 1),
+p1 <- generate_plot(df_HH, y_label = expression("Coefficients of " * delta ~ "(HH)"), 
+                    y_limits = c(-1, 2),
                     facet_labels = c("3" = "(a) Q = 3", "5" = "(b) Q = 5", "7" = "(c) Q = 7"))
 
-ggsave(filename = "../figures/figure_coeff_HH-ANT.pdf", plot = p1, width = 12, height = 2.5)
+p2 <- generate_plot(df_HH_ANT, y_label = expression("Coefficients of " * delta ~ "(HH-ANT)"), 
+                    y_limits = c(-1, 2),
+                    facet_labels = c("3" = "(d) Q = 3", "5" = "(e) Q = 5", "7" = "(f) Q = 7"))
+
+p3 <- grid.arrange(p1, p2, nrow = 2)
+
+ggsave(filename = "../figures/figure_coeff_HH_vs_HH-ANT.pdf", plot = p3, width = 12, height = 5)
